6fe34c7b482bdff52b1362f4304fa38c892ffe2b
AntoniSirena <lic-juansirena@hotmail.com>
2022-05-14 12:32:27 -0400

    update paquete android

--- Movil.App/capacitor.config.ts
+++ Movil.App/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Fast-Inventario',
+  appName: 'Fast-Inv',
   webDir: 'www',
   bundledWebRuntime: false
 };
